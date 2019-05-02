165bda312a74252579a86fc9095fceafe9a9527b
github username <github email address>
2019-05-02 11:58:46 -0400

    CryptoPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppContainer from "./containers/AppContainer"
+
 
 function App() {
   return (
     <div className="App">
-      
+      <AppContainer />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class NavBar extends React.Component {
+
+  render() {
+    return (
+      <div className="nav">
+        <h1>Nav Bar</h1>
+      </div>
+    )
+  }
+
+}
+
+export default NavBar
