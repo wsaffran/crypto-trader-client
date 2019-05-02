09c8ca6b917bd2cd58316fbe0aae8f04a74353c5
Nathaniel Cain <nathaniel2897@gmail.com>
2019-05-02 15:19:08 -0400

    event listeners for nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,18 @@
+import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap';
 
-
-class NavBar extends React.Component {
-  render() {
-    return
+const NavBar = (props) => {
+    return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">CryptoTrader</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-        <Nav.Link href="#learn-more">Learn More</Nav.Link>
-        <Nav.Link href="#browse">Browse</Nav.Link>
-        <Nav.Link href="#user">User</Nav.Link>
+      <Navbar.Brand name="home">CryptoTrader</Navbar.Brand>
+      <Nav className="ml-auto">
+        <Nav.Link onClick={props.handleClick} name="portfolio">Portfolio</Nav.Link>
+        <Nav.Link onClick={props.handleClick} name="browse">Browse</Nav.Link>
+        <Nav.Link onClick={props.handleClick} name="learn-more">Learn More</Nav.Link>
+        <Nav.Link onClick={props.handleClick} name="user">User</Nav.Link>
       </Nav>
     </Navbar>
-    <br />
-  }
+    )
 }
 
 export default NavBar;
-
