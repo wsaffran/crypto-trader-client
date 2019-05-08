0b2d0df0fbb43b6d604b6f09b07ac9c2b6fa49bd
github username <github email address>
2019-05-08 00:14:47 -0400

    cors

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,32 +3,63 @@ import React from 'react'
 class Transaction extends React.Component {
 
   state = {
-    num_of_coins: ''
+    num_of_coins: 0,
+    user_data: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/users/1')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        user_data: data
+      })
+    })
   }
 
   handleChange = (event) => {
     this.setState({
-      num_of_coins: event.target.value
+      num_of_coins: parseInt(event.target.value)
     });
   }
 
   handleSubmit = (event) => {
     event.preventDefault();
-    fetch('http://localhost:3001/users/1/purchases', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        user_id: 1,
-        coin_id: this.props.coinInfo.id,
-        num_of_coins: this.state.num_of_coins,
-        price: this.props.coinInfo.price,
-        time_of_purchase: (new Date().toLocaleString())
+    if (this.state.user_data.balance > (this.props.coinInfo.price * this.state.num_of_coins)) {
+      console.log("We Made It");
+      fetch('http://localhost:3001/users/1/purchases', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accepts': "application/json"
+        },
+        body: JSON.stringify({
+          user_id: 1,
+          coin_id: parseFloat(this.props.coinInfo.id),
+          num_of_coins: parseInt(this.state.num_of_coins),
+          price: parseFloat(this.props.coinInfo.price),
+          time_of_purchase: (new Date().toLocaleString())
+        })
       })
-    })
-    .then(res => res.json())
-    .then(console.log)
+      .then(res => res.json())
+
+      console.log(this.state.user_data.balance)
+      console.log(this.state.num_of_coins)
+      console.log(this.props.coinInfo.price)
+      fetch('http://localhost:3001/users/1', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': "application/json",
+        },
+        body: JSON.stringify(
+          {balance: parseFloat(this.state.user_data.balance) - (parseInt(this.state.num_of_coins) * parseFloat(this.props.coinInfo.price))}
+        )
+      })
+      .then(res => res.json())
+
+
+    }
   }
 
   render() {
